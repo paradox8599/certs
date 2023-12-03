2dbe0853a08425951c15b0bd9d528d04cd5f32d8
paradox8599 <paradox8599@outlook.com>
2023-12-03 17:15:48 +1100

    Fixed typo; add renew log

--- renew.sh
+++ renew.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 docker compose down
+date >> logs/certbot.log
 docker run --rm -it \
     -v ./docker-data/certbot/conf:/etc/letsencrypt \
     -v ./docker-data/certbot/lib:/var/lib/letsencrypt \
     -p "80:80" \
-    certbot/certbot renew
-docker compsoe up -d
+    certbot/certbot renew >> logs/certbot.log
+docker compose up -d
