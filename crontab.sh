ca16f9895c13526ef7eeda673a05dfe77bccc04a
paradox8599 <paradox8599@outlook.com>
2023-12-03 17:51:51 +1100

    crontab script

--- crontab.sh
+++ crontab.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-crontab -l | grep -v "renew.sh" > crontab.tmp
-echo "0 6 * * 1 cd \"$(pwd)\" && ./renew.sh" >> crontab.tmp
+
+crontab -l | grep -v "renew.sh" >crontab.tmp
+if [ "$1" != "rm" ]; then
+    echo "0 6 * * 1 cd \"$(pwd)\" && ./renew.sh" >>crontab.tmp
+    echo "Cron job added"
+else
+    echo "Cron job removed"
+fi
 crontab -r
 crontab crontab.tmp
 rm -rf crontab.tmp
